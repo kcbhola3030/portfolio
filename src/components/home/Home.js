0fce88e8c4ef8c660ed267d0e9c94bd860e4df68
kc <kcbhola123@gmail.com>
2022-09-28 10:06:49 +0530

    remove sidebar from home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,28 +1,13 @@
 import React,{Component} from "react";
 import ReactDOM from 'react-dom';
 import './Home.css';
-import { AiOutlineHome } from 'react-icons/ai';
-import { CgProfile } from 'react-icons/cg';
-import { MdWorkOutline } from 'react-icons/md';
-import { FiPhone } from 'react-icons/fi';
-
-
+import Sidebar from "../sidebar/Sidebar";
 
 
 const Home =()=>{
     return(
         <div>
-           
-        <div className="sidebar">
-            <div className="sidebar2">
-             <div className="sidebar3 d-flex flex-column justify-content-evenly ">
-                <div className="home icon"><AiOutlineHome/></div>
-                <div className="aboutme icon"><CgProfile/></div>
-                <div className="work icon"><MdWorkOutline/></div>
-                <div className="contact icon"><FiPhone/> </div> 
-            </div>
-            </div>
-        </div>
+        <Sidebar/>
         <div className="tilt" >
         <div className="stars"></div>
         <div className="twinkling" ></div> 
